527a34470d019a3aeccfa60e5145eed6c459d43c
Karel Hala <khala@redhat.com>
2020-09-22 08:37:22 +0200

    Pass remediations wizard ref

--- packages/remediations/src/index.js
+++ packages/remediations/src/index.js
@@ -3,14 +3,14 @@ import validate from './validator';
 import RemediationWizard, { getMountedInstance } from './RemediationWizard';
 export { RemediationWizard };
 
-export function openWizard(data, basePath) {
+export function openWizard(data, basePath, wizardRef) {
     const instance = getMountedInstance();
 
-    if (!instance) {
+    if (!instance || !wizardRef?.current) {
         throw new Error('Wizard component not mounted');
     }
 
     validate(data);
 
-    return instance.openWizard(data, basePath);
+    return instance?.openWizard(data, basePath) || wizardRef.current?.openWizard(data, basePath);
 }
